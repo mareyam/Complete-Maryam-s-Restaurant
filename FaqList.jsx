40a41d60d2d53997754ac857f5f7472313f8cb9c
Maryam Naveed <63284481+mareyam@users.noreply.github.com>
2021-09-15 21:41:01 +0500

    Create FaqList.jsx

---
+++ FaqList.jsx
@@ -0,0 +1,46 @@
+
+const FaqList = [
+    {
+        id:1,
+        Question: "What vegetable is also called a 'courgette'.",
+        Answer: "Zucchini."
+    },
+    {
+        id:2,
+        Question: "What shape is the pasta called 'chiocciole'?",
+        Answer: "Shell."
+    },
+    {
+
+        id:3,
+        Question:" What term is used slow cooking meats like duck or pork to slow cook any fat out of them?",
+        Answer: "Rendering"
+    },
+    {   
+        id:4,
+        Question: "Which dish from Spain traditionally includes saffron rice, seafood, chicken, chorizo and peas?",
+        Answer: "Paella."
+    },
+    {
+        id:5,
+        Question: "Which country did brie cheese originate from?",
+        Answer: "France."
+    },
+    {
+        id:6,
+        Question:" What is the name of the first book by chef Anthony Bourdain?",
+        Answer: "'Kitchen Confidential'."
+    },
+    {   
+        id:7,   
+        Question: "What manufacturer introduced Pringles potato chips in 1969?",
+        Answer: "Proctor and Gamble."
+    },
+    {   id:8,  
+        Question:" What is the most commonly used oil in Chinese cooking?",
+        Answer: "Peanut oil."
+    },
+]
+
+export default FaqList;
+
