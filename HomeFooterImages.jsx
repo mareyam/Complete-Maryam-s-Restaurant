b7f50e00e30313f3fd8c28b3b645a2fe49391c67
Maryam Naveed <63284481+mareyam@users.noreply.github.com>
2021-09-15 21:40:08 +0500

    Create HomeFooterImages.jsx

---
+++ HomeFooterImages.jsx
@@ -0,0 +1,33 @@
+
+
+const homeFooterImages = [  //Landing page images
+  {
+    img:"https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1587491227056-AWVLA92JIGORGJLCMF8L/image-asset.jpeg?format=750w", 
+  },
+  {
+    
+    img:"https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1587315142733-7EBE9MSKV9A1YP4C99YK/image-asset.jpeg?format=750w",
+  },
+  {
+    
+    img:"https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1587831132631-A2MQ8LQGGI9VUJGIDLOW/image-asset.jpeg",
+  },
+  {
+    
+    img:"https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1587750601843-JEESLU6MVPS3OK89PQ8I/image-asset.jpeg?format=750w",
+  },
+  {
+    img:"https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1587659847645-JN1Z1Z105VQIWOG64Y79/image-asset.jpeg?format=750w",
+  },
+  {
+    img:"https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1587574515646-NM7FXLTFQNANLP0QYB7M/image-asset.jpeg?format=750w",
+  },
+  {
+    img:"https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1587222204503-PP08R1AX4CHSJUXYYBD1/image-asset.jpeg?format=750w",
+  },
+  {
+    img:"https://images.squarespace-cdn.com/content/v1/590be7fd15d5dbc6bf3e22d0/1587049834086-F1Z94CIH9EYSS1G22QLA/image-asset.jpeg?format=750w",
+  },
+]
+
+export default homeFooterImages;
